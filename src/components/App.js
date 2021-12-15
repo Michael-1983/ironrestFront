2cb894088a5baced65e3fabbd3ed9fd27c13f7da
Michael Sales <michael.fernandes.sales@gmail.com>
2021-12-15 12:28:52 +0000

    Dia 15

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Home from "../pages/Home";
 import Signup from "../pages/auth/Signup";
 import Login from "../pages/auth/Login";
 import ProtectedRoute from "../pages/auth/ProtectedRoute";
 import Navbar from "./Navbar";
-import Posts from "./forms/FormCadastroVitma"
-import { AuthContextComponent } from "../contexts/authContext"
-
+import Posts from "./forms/FormCadastroVitma";
+import { AuthContextComponent } from "../contexts/authContext";
 
 function App() {
   return (
-    <AuthContextComponent>
-      <Navbar />
-    
-      <Routes>
-        <Route path="/" element={<ProtectedRoute component={Home} />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/post" element={<ProtectedRoute component={Posts} />} />
-      </Routes>
-    </AuthContextComponent>
+    <div className="background">
+      <AuthContextComponent>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post" element={<ProtectedRoute component={Posts} />} />
+        </Routes>
+      </AuthContextComponent>
+    </div>
   );
 }
 
