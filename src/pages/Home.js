4be46bd4318cebc711479bd67d722a05b83da239
jfilinho <47835154+jfilinho@users.noreply.github.com>
2021-12-16 09:52:16 -0300

    atualiza fonr

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,11 @@ import React from "react";
 import "../assets/styles/home.css";
 import LoginVitma from "./auth/LoginVitma";
 import LoginFamily from "./auth/LoginFamily";
-import PostList from "./Posts/PostList"
-
 
 function Home() {
   return (
     <>
-    <div>
-      <PostList/>
-      
-      </div> 
+     
        <LoginVitma /> 
  
 
